7389300a1d0c44c5478a36d871e259cc2466cb8c
Adla <wzb23tsu@uea.ac.uk>
2026-01-30 14:58:15 +0000

    Added a new data file and started first R exploration script

---
+++ scripts/initial_exploration.R
@@ -0,0 +1,42 @@
+# Week 1: Initial Data Exploration ====
+# Author: [Adla]
+# Date: [30.01.26]
+
+# Load packages ====
+library(tidyverse)
+library(here)
+library(naniar)
+library(janitor)
+library(skimr)
+# Load data ====
+mosquito_egg_raw <- read_csv(here("data", "mosquito_egg_data.csv"),
+                             name_repair = janitor::make_clean_names)
+
+# Basic overview ====
+glimpse(mosquito_egg_raw)
+summary(mosquito_egg_raw)
+skim(mosquito_egg_raw)
+
+# React table====
+# view interactive table of data
+view(mosquito_egg_raw)
+
+
+# Counts by site and treatment====
+
+mosquito_egg_raw |> 
+  group_by(site, treatment) |> 
+  summarise(n = n())
+
+# Observations ====
+# Your observations (add as comments below):
+# - What biological system is this?
+# Reproductive system   
+# - What's being measured?
+# Measuring the effect of pesticides on egg laying behaviour  
+# - How many observations?
+#   
+# - Anything surprising?
+#   
+# - Any obvious problems?
+#
